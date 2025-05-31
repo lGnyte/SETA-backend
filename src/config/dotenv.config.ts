8eafa6d225c8b75e44e502139e013573d1957ea5
lGnyte <grasu.ioan02@gmail.com>
2025-05-31 22:44:30 +0300

    added cors and integrated authentication

--- src/config/dotenv.config.ts
+++ src/config/dotenv.config.ts
@@ -3,5 +3,5 @@ import { Secret } from 'jsonwebtoken';
 dotenv.config();
 
 export const APP_PORT = process.env.APP_PORT || 3000;
-
+export const CORS_ORIGINS = (process.env.CORS_ORIGINS || '').split(',');
 export const JWT_SECRET :Secret = process.env.JWT_SECRET as Secret;
